21105ccacc5536369a141cc7e72254b0564caa8c
AlexandraLacroix <s294912@ocsb.local>
2018-10-04 13:52:55 -0400

    added Animating Images

--- DisplayingShapesAlexandra/main.lua
+++ DisplayingShapesAlexandra/main.lua
@@ -1,7 +1,14 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title DisplayingShapes  
+-- Name:  Alexandra Lacroix 
+--Course ICS20/3C
+-- This program displays multiple shapes and there names.
 
--- Your code here
+-- create my local variables 
+
+
+-- set the backround color of my screen. 
+ display.setDefault("backround", 47/255, 255/255/, 20/255)
+ -- to remove status bar
+
+ display.setStatusBar
